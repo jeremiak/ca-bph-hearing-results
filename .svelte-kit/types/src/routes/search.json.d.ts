45165c2e951ac17774c9b3c89c81fe23b7c85cc4
Jeremia Kimelman <jbkimelman@gmail.com>
2022-05-22 16:33:37 -0700

    Update .gitignore for svelte kit

--- .svelte-kit/types/src/routes/search.json.d.ts
+++
@@ -1,4 +0,0 @@
-// this file is auto-generated
-import type { RequestHandler as GenericRequestHandler, ResponseBody } from '@sveltejs/kit';
-
-export type RequestHandler<Output extends ResponseBody = ResponseBody> = GenericRequestHandler<{}, Output>;
